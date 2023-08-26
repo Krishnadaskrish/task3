4b1d4fa88ce93af3b46b861fe0a07e952a38f6fb
hornto <krishnadas10.official@gmail.com>
2023-08-26 13:01:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
+
+
 
 function App() {
+  const [post,setPost]=useState([])
+
+  useEffect(()=>{
+  fetch('https://dummyjson.com/quotes')
+  .then(res => res.json())
+  .then(value=>{
+     console.log(value)
+    Array.isArray(value.quotes)?
+  setPost(value.quotes.slice(0,10))
+  :console.log('not avilable')})
+},[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {post.map((post,i)=> 
+      
+     <h1 key={i}>{post.quote}</h1>
+     )}
+      
     </div>
   );
 }
 
+
 export default App;
+
